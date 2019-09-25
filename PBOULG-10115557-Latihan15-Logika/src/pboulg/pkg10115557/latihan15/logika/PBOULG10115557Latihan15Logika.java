80e166aab16749c552ec65aa4f99a268884a6df4
Rayan <37322518+RyanJuan@users.noreply.github.com>
2019-09-25 19:56:07 +0700

    Add files via upload

---
+++ PBOULG-10115557-Latihan15-Logika/src/pboulg/pkg10115557/latihan15/logika/PBOULG10115557Latihan15Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulg.pkg10115557.latihan15.logika;
+
+/**
+ *
+ * @author 10115557 / rayan / PBO Ulg / pertemuan 1 / latihan 15
+ */
+public class PBOULG10115557Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+	boolean b = false;
+	System.out.println("a && b = " + (a && b));
+	System.out.println("a || b = " + (a || b));
+	System.out.println("!a = " + !a);
+	System.out.println("!(a && b) = " + !(a && b));
+	}
+}
